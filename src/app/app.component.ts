236057490951bdbbe7dd421db9404fc39f477bf8
jossrc <kuro.rc10@gmail.com>
2021-01-25 08:46:01 -0500

    Desarrollando un contador o acumulador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Contador App';
+
+  public count: number = 0;
+  public base: number = 5;
+
+  accumulate = (value: number) => {
+    this.count += value;
+  };
 }
