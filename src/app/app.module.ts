cf09fbb5cc071c99e46cde74fab6fb50465c7e64
jossrc <kuro.rc10@gmail.com>
2021-01-25 10:58:51 -0500

    Creando un componente usando el cli de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    HeroComponent
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
