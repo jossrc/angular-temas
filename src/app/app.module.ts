618eeb710c88a0c3cbe823ee54caf8a546050f37
jossrc <kuro.rc10@gmail.com>
2021-01-25 10:13:55 -0500

    Creando el HeroComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html'
+})
+export class HeroComponent {
+
+}
