4e102544f9c3b4bc4053f775a46cf1cd1944c37b
jossrc <kuro.rc10@gmail.com>
2021-01-25 15:08:15 -0500

    Creando módulo para el Counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+
 import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent
-  ],
-  imports: [
-    BrowserModule,
-    HeroesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, CounterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
