17464ffc98933febfbbf7686cd8ac8f2f42beda2
jossrc <kuro.rc10@gmail.com>
2021-01-25 10:26:06 -0500

    Agregando algo de lógica al componente

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  templateUrl: './hero.component.html'
+  templateUrl: './hero.component.html',
 })
 export class HeroComponent {
+  heroName: string = 'Iron Man';
+  age: number = 45;
 
+
+  get capitalizedName() : string {
+    return this.heroName.toUpperCase()
+  }
+
+  getName = (): string => `${this.heroName} - ${this.age}`;
 }
