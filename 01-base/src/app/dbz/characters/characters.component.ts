7060f8e822e93a285b8dd1e1742efa31fa6b1ccf
jossrc <kuro.rc10@gmail.com>
2021-01-26 10:18:36 -0500

    Creando un componente hijo

---
+++ 01-base/src/app/dbz/characters/characters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.css']
+})
+export class CharactersComponent {
+
+  characters: any[] = []
+
+}
