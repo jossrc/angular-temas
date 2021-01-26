a1f4afd82f9872669ebb5cbe9e320b3f4b610ecf
jossrc <kuro.rc10@gmail.com>
2021-01-26 10:19:04 -0500

    Agregando el componente hijo al main

--- 01-base/src/app/dbz/dbz.module.ts
+++ 01-base/src/app/dbz/dbz.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { CharactersComponent } from './characters/characters.component';
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CharactersComponent],
   imports: [CommonModule, FormsModule],
   exports: [MainPageComponent],
 })
