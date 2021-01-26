8b0bfa2b0c00b3e7c179d2bfc74c52a2fce64cb1
jossrc <kuro.rc10@gmail.com>
2021-01-26 09:50:32 -0500

    Uso del ngModel

--- 01-base/src/app/dbz/main-page/main-page.component.ts
+++ 01-base/src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string,
+  power: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  newCharacter: Character = {
+    name: 'Trunks',
+    power: 7000
+  }
+
+
   add = () => {
-    console.log("Esta es una prueba");
+    console.log(this.newCharacter);
+  }
+
+  changeName = (event: any) => {
+    console.log(event.target.value);
   }
 
 }
