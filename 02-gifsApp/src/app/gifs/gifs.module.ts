9faed6fcde9b024e5e396b31e84897dbd6e337a5
jossrc <kuro.rc10@gmail.com>
2021-01-27 12:11:22 -0500

    importando los nuevos componente creados

--- 02-gifsApp/src/app/gifs/gifs.module.ts
+++ 02-gifsApp/src/app/gifs/gifs.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
-
-
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
-  declarations: [GifsPageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [GifsPageComponent]
+  declarations: [GifsPageComponent, SearchComponent, ResultsComponent],
+  imports: [CommonModule],
+  exports: [GifsPageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
