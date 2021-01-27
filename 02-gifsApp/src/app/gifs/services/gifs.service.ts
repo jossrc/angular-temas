0cf672d0762cd2d407116b3806b2852b7dde7d0d
jossrc <kuro.rc10@gmail.com>
2021-01-27 18:00:05 -0500

    Realizando una petición HTTP

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _history: string[] = [];
-
+  private _url: string = 'https://api.giphy.com/v1/gifs/search';
+  private _apiKey: string = '';
 
   /**
    * Retorna un arreglo de todas las busquedas realizadas
@@ -14,7 +16,7 @@ export class GifsService {
     return [...this._history];
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   /**
    * Busca un gif mediante la API de Giphy y
@@ -26,6 +28,13 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
     }
+
+    const fullUrl = `${this._url}?api_key=${this._apiKey}&q=${encodeURIComponent(query)}&limit=10`;
+
+    this.http.get(fullUrl).subscribe((response: any) => {
+      console.log(response.data);
+    });
+
     console.log(this._history);
   };
 }
