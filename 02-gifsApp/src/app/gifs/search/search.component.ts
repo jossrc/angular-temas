3faf4052e6489cf2f536db7a6c96392de3e6d6c8
jossrc <kuro.rc10@gmail.com>
2021-01-27 15:14:54 -0500

    Uso del viewchild

--- 02-gifsApp/src/app/gifs/search/search.component.ts
+++ 02-gifsApp/src/app/gifs/search/search.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
-  styles: [
-  ]
+  templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  /*referencia local # */
+  // #txtSearch == document.querySelector('input') parecido
+  // Puede usar como argumento en el (keyup.enter) = method(txtSearch)
 
-  ngOnInit(): void {
+
+  /* Non-null assertion operator !: */
+
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+
+  searchGifs = ( contentSearch: string ) => {
+
+    //const value = this.txtSearch.nativeElement.value
+
+    console.log(contentSearch);
+    console.log(this.txtSearch);
+    //console.log(value);
+
+    this.txtSearch.nativeElement.value = ''
   }
+
 
 }
