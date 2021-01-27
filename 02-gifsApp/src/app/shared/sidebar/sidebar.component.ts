968478d5dcb9d61a2f322c98c7b4e60473dec88b
jossrc <kuro.rc10@gmail.com>
2021-01-27 16:02:55 -0500

    Usando el GifsService para agregar las busquedas

--- 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get history(): string[] {
+    return this.gifsService.history;
   }
 
+  constructor(private gifsService: GifsService) {}
+
+  ngOnInit(): void {}
 }
