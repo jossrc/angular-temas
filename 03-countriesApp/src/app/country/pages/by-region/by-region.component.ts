71ba1b79b947a828693aaa9df3cee3347a94fd78
jossrc <kuro.rc10@gmail.com>
2021-02-01 11:55:50 -0500

    Clases de CSS condicionales

--- 03-countriesApp/src/app/country/pages/by-region/by-region.component.ts
+++ 03-countriesApp/src/app/country/pages/by-region/by-region.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  getCSSClass = (region: string) =>
+    region === this.activeRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
 
+  activateRegion = (region: string) => {
+    this.activeRegion = region;
+    // TODO: Hacer el llamado al servicio
+  };
 }
