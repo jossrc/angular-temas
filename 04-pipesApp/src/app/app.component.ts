4bfefbcb2f9508d68fb0829ecdc92f02c0c908a8
jossrc <kuro.rc10@gmail.com>
2021-02-16 08:31:41 -0500

    Iniciando con el tema de pipes

---
+++ 04-pipesApp/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  name: string = 'Jose robLeS';
+  value: number = 1000;
+
+  obj = {
+    name: this.name
+  }
+
+  showName() {
+    console.log(`Hi, my name is ${this.name}`);
+  }
+}
