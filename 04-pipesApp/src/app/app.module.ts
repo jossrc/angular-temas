af6be96f1ee7f41f2df4c719cff703b9350383d2
jossrc <kuro.rc10@gmail.com>
2021-02-16 11:43:02 -0500

    PrimeNg module

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-// PrimeNG
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
+// Módulo personalizado
+import {PrimeNgModule} from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import { CardModule } from 'primeng/card';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
