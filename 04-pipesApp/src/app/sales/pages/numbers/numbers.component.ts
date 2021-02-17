4bf17a6a79388b7a446436fd8696fc73420d4763
jossrc <kuro.rc10@gmail.com>
2021-02-17 12:46:24 -0500

    DecimalPipe

--- 04-pipesApp/src/app/sales/pages/numbers/numbers.component.ts
+++ 04-pipesApp/src/app/sales/pages/numbers/numbers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  totalSales = 2575488457.18746;
+  inPercentage = 0.48;
 
 }
