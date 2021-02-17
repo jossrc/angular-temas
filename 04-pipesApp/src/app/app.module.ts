05cac2445e7de187e538f49ca2bec7026a841686
jossrc <kuro.rc10@gmail.com>
2021-02-17 11:26:07 -0500

    Timezone y otros idiomas

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -10,9 +10,11 @@ import { SalesModule } from './sales/sales.module';
 
 // Cambiar el locale de la app
 import localeEs from '@angular/common/locales/es-PE';
+import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [AppComponent],
