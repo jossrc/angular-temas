abe65bddccd7e261d809060932be6c5911a72ee9
jossrc <kuro.rc10@gmail.com>
2021-02-17 10:23:40 -0500

    Cambiar el idioma por defecto

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,10 +8,18 @@ import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { SalesModule } from './sales/sales.module';
 
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, SalesModule],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-PE'}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
