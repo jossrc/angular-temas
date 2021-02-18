e68bf5b1cf9b93e0ca95ef26a06133b5b3a4730c
jossrc <kuro.rc10@gmail.com>
2021-02-18 16:20:35 -0500

    Llenar un PrimeTable con data

--- 04-pipesApp/src/app/sales/pages/order/order.component.ts
+++ 04-pipesApp/src/app/sales/pages/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Color, Hero} from '../../interfaces/sales.interfaces';
 
 @Component({
   selector: 'app-order',
@@ -9,8 +10,33 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   enableUppercase = true;
-
-  constructor() { }
+  heroes: Hero[] = [
+    {
+      name: 'Superman',
+      fly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      color: Color.black
+    },
+    {
+      name: 'Robin',
+      fly: false,
+      color: Color.green
+    },
+    {
+      name: 'Daredevil',
+      fly: false,
+      color: Color.red
+    },
+    {
+      name: 'Linterna Verde',
+      fly: true,
+      color: Color.green
+    }
+  ];
 
   toggleUppercase(): void {
     this.enableUppercase = !this.enableUppercase;
