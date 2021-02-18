5fcf6dd6fe23413eedb1ca322a729a9930b3779e
jossrc <kuro.rc10@gmail.com>
2021-02-18 15:46:19 -0500

    Agregando el ToolbarModule

--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -5,13 +5,15 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
     FieldsetModule,
-    MenubarModule
+    MenubarModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
