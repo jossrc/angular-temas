61daa2ed4d69e24ff491799e06f715ad1ca1aec3
jossrc <kuro.rc10@gmail.com>
2021-03-18 13:39:09 -0500

    Creando el app-routing.module.ts

---
+++ 05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
