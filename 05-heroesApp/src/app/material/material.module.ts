98580bdfe8079c70ccbcd93b89550d00583541fb
jossrc <kuro.rc10@gmail.com>
2021-03-19 17:21:44 -0500

    Material Card - Flex Layout

--- 05-heroesApp/src/app/material/material.module.ts
+++ 05-heroesApp/src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatCardModule
   ],
 })
 export class MaterialModule {}
