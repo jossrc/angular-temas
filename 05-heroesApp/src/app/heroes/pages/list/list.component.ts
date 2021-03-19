743af498090480ed5f36a976b601283f80974548
jossrc <kuro.rc10@gmail.com>
2021-03-19 17:31:44 -0500

    Flex Layout - Diferentes Resoluciones

--- 05-heroesApp/src/app/heroes/pages/list/list.component.ts
+++ 05-heroesApp/src/app/heroes/pages/list/list.component.ts
@@ -5,7 +5,13 @@ import { Hero } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styles: [],
+  styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ListComponent implements OnInit {
   public heroList: Hero[] = [];
