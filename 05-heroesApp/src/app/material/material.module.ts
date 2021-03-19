6c432cf07a369c6608cc839a3a79a927ab8724ea
jossrc <kuro.rc10@gmail.com>
2021-03-19 10:42:20 -0500

    Material Navlist

--- 05-heroesApp/src/app/material/material.module.ts
+++ 05-heroesApp/src/app/material/material.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
