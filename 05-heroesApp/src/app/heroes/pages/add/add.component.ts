0d2ec6db877b922d316a513ad56afa60b360b420
jossrc <kuro.rc10@gmail.com>
2021-03-22 10:30:22 -0500

    Diseño de la pantalla para agregar héroes

--- 05-heroesApp/src/app/heroes/pages/add/add.component.ts
+++ 05-heroesApp/src/app/heroes/pages/add/add.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AddComponent implements OnInit {
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics',
+    },
+  ];
 
-  constructor() { }
+  hero: Hero = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
