d57087be45031b4df70798be9332127a383a9b27
jossrc <kuro.rc10@gmail.com>
2021-03-26 10:35:52 -0500

    Pantalla de Login básico

--- 05-heroesApp/src/app/auth/auth.module.ts
+++ 05-heroesApp/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import {MaterialModule} from '../material/material.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
