d54d7e2f63dbf53cbaa6812093c9c7d9d60a2fc2
jossrc <kuro.rc10@gmail.com>
2021-03-29 18:44:52 -0500

    Template - FormsModule

--- 06-formsApp/src/app/template/basic/basic.component.ts
+++ 06-formsApp/src/app/template/basic/basic.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
   styles: [
-  ]
+    `
+      pre {
+        background: #fafafa;
+        border: 1px solid #fefefe;
+        padding: 5px;
+      }
+    `,
+  ],
 })
 export class BasicComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  save(myForm: NgForm): void {
+    console.log(myForm.value);
   }
-
 }
--- 06-formsApp/src/app/template/template.module.ts
+++ 06-formsApp/src/app/template/template.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicComponent } from './basic/basic.component';
 import { DinamicComponent } from './dinamic/dinamic.component';
 import { SwitchesComponent } from './switches/switches.component';
 
-
 @NgModule({
   declarations: [BasicComponent, DinamicComponent, SwitchesComponent],
   imports: [
     CommonModule,
-    TemplateRoutingModule
+    TemplateRoutingModule,
+    FormsModule
   ]
 })
 export class TemplateModule { }
