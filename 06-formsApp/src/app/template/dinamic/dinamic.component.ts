6dab596bccba341a54f2eb55c7139fb321770cf7
jossrc <kuro.rc10@gmail.com>
2021-05-05 16:37:04 -0500

    Avanzando con el formulario dinámico

--- 06-formsApp/src/app/template/dinamic/dinamic.component.ts
+++ 06-formsApp/src/app/template/dinamic/dinamic.component.ts
@@ -18,12 +18,29 @@ interface Favorite {
 })
 export class DinamicComponent {
 
+  newGame: string = "";
+
   person: Person = {
     name: 'Jose Robles',
     favorites: [
       { id: 1, name: 'Dark Souls' },
       { id: 2, name: 'Bloodborne' },
     ]
+  }
+
+  addNewGame(): void {
+    const favorite: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newGame
+    }
+
+    this.person.favorites.push({...favorite});
+    this.newGame = '';
+
+  }
+
+  remove(index: number): void {
+    this.person.favorites.splice(index, 1);
   }
 
   save(): void {
