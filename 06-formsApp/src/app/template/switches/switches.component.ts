b0ac38f1053949e0003d83a853a7942c659856d5
jossrc <kuro.rc10@gmail.com>
2021-05-06 12:27:31 -0500

    Validando radios, checks y switches

--- 06-formsApp/src/app/template/switches/switches.component.ts
+++ 06-formsApp/src/app/template/switches/switches.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  person = {
+    gender: '',
+    notifications: true,
   }
+
+  termsAndConditions: boolean = false;
 
 }
