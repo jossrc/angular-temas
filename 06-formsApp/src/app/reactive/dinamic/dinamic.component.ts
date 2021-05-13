0117e566056dc96369c4ebee2a73bbf150e577f9
jossrc <kuro.rc10@gmail.com>
2021-05-13 11:39:28 -0500

    Validando campos

--- 06-formsApp/src/app/reactive/dinamic/dinamic.component.ts
+++ 06-formsApp/src/app/reactive/dinamic/dinamic.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamic',
   templateUrl: './dinamic.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicComponent implements OnInit {
+export class DinamicComponent {
+  myForm: FormGroup = this.formBuilder.group({
+    txtPersonName: ['', [Validators.required, Validators.minLength(3)]],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
   }
 
+  save(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 }
