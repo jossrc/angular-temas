9ddfbf9061ba41b880f078765489221998a18dfc
jossrc <kuro.rc10@gmail.com>
2021-05-14 11:43:32 -0500

    switches

--- 06-formsApp/src/app/reactive/switches/switches.component.ts
+++ 06-formsApp/src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.formBuilder.group({
+    rdoGender: ['F', Validators.required],
+    chkNotifications: [false, Validators.required],
+    //chkConditions: [false, Validators.required],
+    chkConditions: [false, Validators.requiredTrue]
+  });
 
+  person = {
+    gender: 'M',
+    notifications: true,
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  // Debemos tener cuidado ya que cuando no se establece será null e inválido
   ngOnInit(): void {
+    this.myForm.reset({
+      rdoGender: this.person.gender,
+      chkNotifications: this.person.notifications,
+      chkConditions: false,
+    });
   }
-
 }
