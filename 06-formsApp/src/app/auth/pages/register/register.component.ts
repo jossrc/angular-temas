5d03da72c32b43593e7e9b51a03ab5fea832ef33
jossrc <kuro.rc10@gmail.com>
2021-05-15 15:11:13 -0500

    Evaluar un email

--- 06-formsApp/src/app/auth/pages/register/register.component.ts
+++ 06-formsApp/src/app/auth/pages/register/register.component.ts
@@ -9,17 +9,28 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   //TODO: Temporal
   fullNamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   myForm: FormGroup = this.formBuilder.group({
     txtName: [
       '',
       [Validators.required, Validators.pattern(this.fullNamePattern)],
     ],
+    txtEmail: [
+      '',
+      [Validators.required, Validators.pattern(this.emailPattern)],
+    ],
   });
 
   constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Estableciendo valores por defecto
+    this.myForm.reset({
+      txtName: 'Jose Robles',
+      txtEmail: 'test1@test.com',
+    });
+  }
 
   isValidField(field: string) {
     return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched;
