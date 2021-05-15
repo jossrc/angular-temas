76d01272132eeee232236246aa46fe12c718b5c3
jossrc <kuro.rc10@gmail.com>
2021-05-15 14:49:20 -0500

    Validar con expresión regular

--- 06-formsApp/src/app/auth/auth.module.ts
+++ 06-formsApp/src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- 06-formsApp/src/app/auth/pages/register/register.component.ts
+++ 06-formsApp/src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  //TODO: Temporal
+  fullNamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  myForm: FormGroup = this.formBuilder.group({
+    txtName: [
+      '',
+      [Validators.required, Validators.pattern(this.fullNamePattern)],
+    ],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  isValidField(field: string) {
+    return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched;
+  }
+
+  submitForm() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      return;
+    }
+
+    this.myForm.markAllAsTouched();
+  }
 }
