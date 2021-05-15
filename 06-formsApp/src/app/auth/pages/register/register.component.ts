0794416f6ed20d07aa463f43caa54f170bffeebe
jossrc <kuro.rc10@gmail.com>
2021-05-15 16:01:37 -0500

    Validaciones personalizadas

--- 06-formsApp/src/app/auth/pages/register/register.component.ts
+++ 06-formsApp/src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +16,24 @@ export class RegisterComponent implements OnInit {
   fullNamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
+  isValidUsername(control: FormControl) {
+    const value: string | null = control.value?.trim().toLowerCase();
+    if (value === 'strider') {
+      // Sí hay error - objeto
+      return {
+        isStrider: true,
+      };
+    }
+    // No hay error
+    return null;
+  }
+
   myForm: FormGroup = this.formBuilder.group({
     txtName: [
       '',
       [Validators.required, Validators.pattern(this.fullNamePattern)],
     ],
+    txtUsername: ['', [Validators.required, this.isValidUsername]],
     txtEmail: [
       '',
       [Validators.required, Validators.pattern(this.emailPattern)],
@@ -28,6 +46,7 @@ export class RegisterComponent implements OnInit {
     // Estableciendo valores por defecto
     this.myForm.reset({
       txtName: 'Jose Robles',
+      txtUsername: 'jossrc',
       txtEmail: 'test1@test.com',
     });
   }
