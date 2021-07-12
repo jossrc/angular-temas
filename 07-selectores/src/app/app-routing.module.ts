3618ece397c5b62788afd7adb8fb352a983cf97e
jossrc <kuro.rc10@gmail.com>
2021-07-12 15:42:40 -0500

    Estructura de directorios para la aplicación

---
+++ 07-selectores/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'selector',
+    loadChildren: () =>
+      import('./paises/paises.module').then((module) => module.PaisesModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'selector'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ 07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+})
+export class SelectorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
