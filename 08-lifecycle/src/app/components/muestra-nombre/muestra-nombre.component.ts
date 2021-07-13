a61596c622e4eb7d79fb54332c47bb13074794ea
jossrc <kuro.rc10@gmail.com>
2021-07-13 14:37:16 -0500

    Inicio de proyecto

---
+++ 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [
+  ]
+})
+export class MuestraNombreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
