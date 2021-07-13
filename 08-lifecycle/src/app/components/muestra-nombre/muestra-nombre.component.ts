f812dad90d0538db873949e323ff9023d0eb556c
jossrc <kuro.rc10@gmail.com>
2021-07-13 16:42:58 -0500

    ngOnChanges

--- 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
+++ 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-muestra-nombre',
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class MuestraNombreComponent implements OnInit {
+export class MuestraNombreComponent implements OnInit, OnChanges {
+
+  @Input()
+  nombre!: string;
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
   ngOnInit(): void {
+
   }
 
 }
