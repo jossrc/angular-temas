bcd9adf99334f103d0911cb7bc7d932784c9e0d0
jossrc <kuro.rc10@gmail.com>
2021-07-13 14:57:06 -0500

    Implementando todos los hooks del ciclo de vida

--- 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
+++ 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
@@ -6,11 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component implements OnInit, OnChanges, DoCheck ,AfterContentInit,
+  AfterContentChecked, AfterViewInit,AfterViewChecked, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /*
+    En el constructor hacemos inyección de dependencias que el
+    componente necesita o si necesitamos una inicialización
+    antes que el html sea construido
+  */
+  constructor() {
+    console.log('constructor()');
   }
 
+  /*
+    Es un hook, se utiliza cuando un componente está inicializado,
+    cuando se crea o cambia la información del padre como traer peticiones Http.
+    Traer información de servicios
+  */
+  ngOnInit(): void {
+    console.log('ngOnInit()');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges()');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck()');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit()');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked()');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit()');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked()');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy()');
+  }
 }
