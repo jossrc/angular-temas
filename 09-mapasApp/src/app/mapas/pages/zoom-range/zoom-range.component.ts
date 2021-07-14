cace6bc5c862b76030e474cb1c5a937e82d0f610
jossrc <kuro.rc10@gmail.com>
2021-07-14 11:24:52 -0500

    Controlar el objeto del mapa - ZoomIn y ZoomOut

--- 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .row {
+        background-color: white;
+        border-radius: 15px;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        z-index: 999;
+        -webkit-box-shadow: 4px 8px 15px 0px rgba(0, 0, 0, 0.78);
+        box-shadow: 4px 8px 15px 0px rgba(0, 0, 0, 0.78);
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
+  // Obtenemos el elemento <div #mapa> por su referencia
+  @ViewChild('mapa')
+  mapaContainer!: ElementRef;
 
-  ngOnInit(): void {
+  miMapa!: mapboxgl.Map;
+
+  constructor() {}
+
+  /**
+   * Se requiere de este método debido a que necesitamos obtener el elemento
+   * (<div #mapa>) cuando la vista se haya cargado por completo (después del onInit)
+   **/
+  ngAfterViewInit(): void {
+    this.miMapa = new mapboxgl.Map({
+      // El container también puede recibir un elemento html (tener cuidado con el ID)
+      container: this.mapaContainer.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-77.0296675277514, -12.120856900175406],
+      zoom: 17,
+    });
+  }
+
+  zoomOut(): void {
+    // Alejar
+    this.miMapa.zoomOut();
+  }
+
+  zoomIn(): void {
+    // Acercar
+    this.miMapa.zoomIn();
   }
 
 }
