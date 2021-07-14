c72be91100c17a6ecef0fb6e6f0a783661549507
jossrc <kuro.rc10@gmail.com>
2021-07-14 09:24:36 -0500

    Mostrar un mapa en pantalla completa

--- 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
+++ 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
   styles: [
-  ]
+    `
+    #mapa {
+      width: 100%;
+      height: 100%
+    }
+    `
+  ],
 })
 export class FullScreenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
+  /**
+   * Para que funcione es necesario establecer un ancho y alto al body,
+   * también el contenedor debe tener su ancho y alto
+   *
+   * */
   ngOnInit(): void {
+    // Se establece el token de acceso
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      // Se agrega el id del contenedor
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+    });
   }
-
 }
