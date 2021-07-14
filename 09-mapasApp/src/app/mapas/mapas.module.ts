1461ab220055bdad709783100b62ac3979206a3c
jossrc <kuro.rc10@gmail.com>
2021-07-14 07:08:26 -0500

    Creando los componentes necesarios y rutas

---
+++ 09-mapasApp/src/app/mapas/mapas.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapasRoutingModule } from './mapas-routing.module';
+import { MiniMapaComponent } from './components/mini-mapa/mini-mapa.component';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+import { MenuComponent } from './shared/menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    MiniMapaComponent,
+    FullScreenComponent,
+    MarcadoresComponent,
+    ZoomRangeComponent,
+    PropiedadesComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    MapasRoutingModule
+  ]
+})
+export class MapasModule { }
