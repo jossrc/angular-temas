e4038da97b2a0ed4e31d7292d8c7b304e346c178
jossrc <kuro.rc10@gmail.com>
2021-07-16 14:25:38 -0500

    Grafica de dona mediante petición Http

--- 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DonaHttpComponent implements OnInit {
-
-  constructor() { }
+  constructor(private graficasService: GraficasService) {}
 
   ngOnInit(): void {
+    this.graficasService.getUsuariosRedesSociales().subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
