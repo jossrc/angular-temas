1ff2ede3b5312058be3ab224c7bba3ace7961f12
jossrc <kuro.rc10@gmail.com>
2021-07-15 20:22:56 -0500

    Menú de aplicación

--- 10-graficasApp/src/app/shared/menu/menu.component.ts
+++ 10-graficasApp/src/app/shared/menu/menu.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
-  ]
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menu: MenuItem[] = [
+    { ruta: '/graficas/barra', texto: 'Barras' },
+    { ruta: '/graficas/barra-doble', texto: 'Barras Dobles' },
+    { ruta: '/graficas/dona', texto: 'Dona' },
+    { ruta: '/graficas/dona-http', texto: 'Dona Http' },
+  ];
 }
