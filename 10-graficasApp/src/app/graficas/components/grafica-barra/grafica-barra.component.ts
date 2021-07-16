68e6079e309ad1ae0537c26d740bb49449e020d5
jossrc <kuro.rc10@gmail.com>
2021-07-15 22:10:26 -0500

    Componente personalizado para mostrar gráficos

--- 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -7,6 +9,45 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent implements OnInit {
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public barChartLabels: Label[] = [
+    '2020',
+    '2021',
+    '2022',
+    '2023',
+    '2024',
+    '2025',
+    '2026',
+  ];
+
+  public barChartType: ChartType = 'bar';
+
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[] = [
+    {
+      data: [65, 59, 80, 81, 56, 55, 40],
+      label: 'Series A',
+      backgroundColor: '#DB8AC5',
+      hoverBackgroundColor: 'pink',
+    },
+    {
+      data: [28, 48, 40, 19, 86, 27, 90],
+      label: 'Series B',
+      backgroundColor: '#7783F0',
+      hoverBackgroundColor: 'blue',
+    },
+    {
+      data: [8, 38, 70, 59, 66, 80, 100],
+      label: 'Series C',
+      backgroundColor: '#93DBC3',
+      hoverBackgroundColor: 'green',
+    },
+  ];
 
   constructor() { }
 
