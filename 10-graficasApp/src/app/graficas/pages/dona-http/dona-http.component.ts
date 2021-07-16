2a8fbc33575e54713e75554de9e5bff8b8e1c0a1
jossrc <kuro.rc10@gmail.com>
2021-07-16 14:48:00 -0500

    Mostrar la información en la grafica

--- 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -7,11 +9,29 @@ import { GraficasService } from '../../services/graficas.service';
   styles: [],
 })
 export class DonaHttpComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = [];
+
+  public doughnutChartData: MultiDataSet = [];
+
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    { backgroundColor: ['#85F0BB', '#95A7DB', '#E9EB9D', '#EBB08D'] }
+  ];
+
   constructor(private graficasService: GraficasService) {}
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales().subscribe((data) => {
       console.log(data);
+      // Obtengo todas las llaves del objeto como un arreglo
+      const labels = Object.keys(data);
+      // Obtengo todas los valores del objeto como un arreglo
+      const values = Object.values(data);
+
+      this.doughnutChartLabels = labels;
+      this.doughnutChartData.push(values);
     });
   }
 }
