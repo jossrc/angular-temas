4c7ff0425c246bce11b0fc3b5c19e7b36f151bd7
jossrc <kuro.rc10@gmail.com>
2021-07-17 12:17:45 -0500

    Validando el post login

--- 12-auth-server/controllers/auth.js
+++ 12-auth-server/controllers/auth.js
@@ -1,5 +1,6 @@
 // Agregar tipado en javascript (hacks)
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 
@@ -13,7 +14,17 @@ const crearUsuario = (req, res = response) => {
   });
 };
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+
+  // Verificamos si existen errores (express-validator)
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+  }
 
   const { email, password } = req.body;
   console.log(email, password);
--- 12-auth-server/routes/auth.js
+++ 12-auth-server/routes/auth.js
@@ -1,14 +1,21 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidar } = require('../controllers/auth');
 
 // Usando el Router para crear rutas
 const router = Router();
 
-// Ruta Post para crear un nuevo usuario con la referencia de su controller
+// Ruta Post para crear un nuevo usuario con sus middlewares y
+// la referencia de su controller
 router.post('/new', crearUsuario);
 
-// Ruta Post para el login (vacío) con la referencia de su controller
-router.post('', loginUsuario);
+// Ruta Post para el login (vacío) con sus middlewares y
+// la referencia de su controller
+router.post('', [
+  // Validando campo email y password (express-validator)
+  check('email', 'El email es obligatorio').isEmail(),
+  check('password', 'La contraseña es obligatoria').isLength({min: 6})
+] ,loginUsuario);
 
 // Ruta para validar y revalidar token
 router.get('/renew', revalidar );
