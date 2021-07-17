36b0c46e64aa970704bea798a74be262b9ac40e7
jossrc <kuro.rc10@gmail.com>
2021-07-17 11:02:23 -0500

    Configurar CORS y body de las peticiones

--- 12-auth-server/controllers/auth.js
+++ 12-auth-server/controllers/auth.js
@@ -2,6 +2,11 @@
 const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
+
+  // Cuando está parseado el body podemos trabajar con json
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+
   return res.json({
     ok: true,
     msg: 'Crear usuario /new',
@@ -9,6 +14,10 @@ const crearUsuario = (req, res = response) => {
 };
 
 const loginUsuario = (req, res) => {
+
+  const { email, password } = req.body;
+  console.log(email, password);
+
   return res.json({
     ok: true,
     msg: 'Login de usuario /',
--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 // Crear el servidor/aplicación de express
 const app = express();
+
+// CORS 
+app.use(cors());
+
+// Lectura y parseo del body - permitir trabajar la request con json
+app.use(express.json()); // -> implementar antes que las rutas 
 
 // Manejando las rutas con middlewares (use)
 // La ruta /api/auth se concatenará con la dirección en los routes
