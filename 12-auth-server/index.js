bd292787d117b5350746d34bf048abbc4b0c7969
jossrc <kuro.rc10@gmail.com>
2021-07-17 09:57:42 -0500

    Configurar servidor de Express

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,1 +1,20 @@
-console.log('Hola desde Node')
+const express = require('express');
+
+// Crear el servidor/aplicación de express
+const app = express();
+
+// Creando Petición GET
+app.get('/', (req, res) => {
+  console.log('Petición en el /');
+  // Mandando una respuesta
+  res.status(200).json({
+    ok: true,
+    mensaje: 'Todo bien',
+    uuid: 1234,
+  });
+});
+
+// Escuchando en el puerto 4000
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en el puerto ${4000}`);
+});
