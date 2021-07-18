9e186b33e8340791b383352c87a0faa4d420304c
jossrc <kuro.rc10@gmail.com>
2021-07-18 16:30:24 -0500

    Crear usuario en base de datos

--- 12-auth-server/controllers/auth.js
+++ 12-auth-server/controllers/auth.js
@@ -1,21 +1,52 @@
 // Agregar tipado en javascript (hacks)
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
   // Cuando está parseado el body podemos trabajar con json
   const { name, email, password } = req.body;
-  console.log(name, email, password);
 
-  return res.json({
-    ok: true,
-    msg: 'Crear usuario /new',
-  });
+  try {
+    // Verificar si existe un email igual en la base de datos
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El email ya está en uso',
+      });
+    }
+
+    // Crear usuario con el modelo
+    usuario = new Usuario(req.body);
+
+    // Encriptar (Hashear) la contraseña
+
+    // Generar el JWT
+
+    // Crear usuario de DB
+    await usuario.save();
+
+    // Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: usuario.id, // el id es el uid que genera mongo
+      name,
+      msg: 'Usuario creado exitosamente',
+    });
+  } catch (err) {
+    // Se recomienda mostrar el error en la consola
+    console.log(err);
+    // Se envía un mensaje al cliente (a elección)
+    return res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador',
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
-  console.log(email, password);
 
   return res.json({
     ok: true,
