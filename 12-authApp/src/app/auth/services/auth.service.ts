8bb947a6cd45fc1896bdc67d093ac8063d7ecc58
jossrc <kuro.rc10@gmail.com>
2021-07-19 16:54:02 -0500

    Logout

--- 12-authApp/src/app/auth/services/auth.service.ts
+++ 12-authApp/src/app/auth/services/auth.service.ts
@@ -67,4 +67,9 @@ export class AuthService {
       catchError((err: HttpErrorResponse) => of(false))
     );
   }
+
+  logout() {
+    localStorage.clear();
+  }
+
 }
--- 12-authApp/src/app/protected/dashboard/dashboard.component.ts
+++ 12-authApp/src/app/protected/dashboard/dashboard.component.ts
@@ -28,6 +28,7 @@ export class DashboardComponent {
 
   logout() {
     this.router.navigateByUrl('/auth')
+    this.authService.logout();
   }
 
 }
